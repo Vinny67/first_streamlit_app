c80e4ac816c7e89bebe6924ea78dc3c037fda978
Vinny67 <122836303+Vinny67@users.noreply.github.com>
2023-02-10 10:18:51 +0530

    Update streamli_app.py
    changed few things

--- streamli_app.py
+++ streamli_app.py
@@ -1,8 +1,8 @@
 import streamlit
 streamlit.title('my parents healthy breakfast')
 streamlit.header('Breakfast Menu')
-streamlit.text(🥣'Omega 3 & Blueberry Oatmeal')
-streamlit.text( 🥗 'Kale, Spinach & Rocket Smoothie')
-streamlit.text( 🐔 'Hard-Boiled Free-Range Egg')
-streamlit.text( 🥑🍞 'avacado toast')
+streamlit.text('🥣Omega 3 & Blueberry Oatmeal')
+streamlit.text(  '🥗Kale, Spinach & Rocket Smoothie')
+streamlit.text(  '🐔Hard-Boiled Free-Range Egg')
+streamlit.text( ' 🥑🍞avacado toast')
 
