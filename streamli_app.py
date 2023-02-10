ccc57f0d3bab068fbe10c75b25679d7a466facc0
Vinny67 <122836303+Vinny67@users.noreply.github.com>
2023-02-10 10:12:19 +0530

    Update streamli_app.py

--- streamli_app.py
+++ streamli_app.py
@@ -1,1 +1,6 @@
-TRYBSOLKM SDNAKNASLJKDN
+import streamlit
+streamlit.title('my parents healthy breakfast')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
