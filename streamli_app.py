6d4bd5e6659c92b02862df67f75c00b76cd4f302
Vinny67 <122836303+Vinny67@users.noreply.github.com>
2023-02-10 10:17:38 +0530

    Update streamli_app.py
    added avocado toast

--- streamli_app.py
+++ streamli_app.py
@@ -1,6 +1,8 @@
 import streamlit
 streamlit.title('my parents healthy breakfast')
 streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.text(🥣'Omega 3 & Blueberry Oatmeal')
+streamlit.text( 🥗 'Kale, Spinach & Rocket Smoothie')
+streamlit.text( 🐔 'Hard-Boiled Free-Range Egg')
+streamlit.text( 🥑🍞 'avacado toast')
+
